63014a8a1a65d693286964f0083a244f7c0ddf33
IfyObiakor <114431584+IfyObiakor@users.noreply.github.com>
2023-07-18 14:35:51 +0100

    update

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ fetch('https://random-data-api.com/api/v2/users?size=15&is_xml=true', {
   data.forEach(user => {
     const row = document.createElement('tr');
     row.innerHTML = `
-      <td>${user.first_name}</td>
-      <td>${user.last_name}</td>
-      <td>${user.email}</td>
-      <td>${user.subscription.plan}</td>
+      <td class="px-6 py-3 border">${user.first_name}</td>
+      <td class="px-6 py-3 border">${user.last_name}</td>
+      <td class="px-6 py-3 border">${user.email}</td>
+      <td class="px-6 py-3 border">${user.subscription.plan}</td>
     `;
     userTableBody.appendChild(row);
   });
